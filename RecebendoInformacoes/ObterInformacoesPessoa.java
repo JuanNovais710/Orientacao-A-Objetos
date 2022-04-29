1222e3d527f65e370e1263dca1ca21854f1fa545
JuanNovais710 <100390136+JuanNovais710@users.noreply.github.com>
2022-04-29 16:35:49 -0300

    Add files via upload

--- RecebendoInformacoes/ObterInformacoesPessoa.java
+++ RecebendoInformacoes/ObterInformacoesPessoa.java
@@ -1,6 +1,4 @@
 package orientacao.objetos.produto.RecebendoInformacoes;
-
-import javax.swing.*;
 import java.util.Scanner;
 
 public class ObterInformacoesPessoa {
@@ -8,20 +6,21 @@ public class ObterInformacoesPessoa {
         Pessoa pessoa = new Pessoa();
         Scanner scanner = new Scanner(System.in);
         imprimirTexto("Informe seu primeiro nome: ");
-        pessoa.nome = scanner.nextLine();
+        pessoa.setNome(scanner.nextLine());
         imprimirTexto("Olá, " + pessoa.nome + "! E o seu sobrenome? ");
-        pessoa.sobrenome = scanner.nextLine();
+        pessoa.setSobrenome(scanner.nextLine());
         imprimirTexto(pessoa.formarNomeCompleto() + ", favor informar sua idade: ");
-        pessoa.idade = scanner.nextByte();
-        imprimirTexto("Seu telefone com DDD: ");
-        pessoa.numeroTelefone = scanner.nextLong();
-        imprimirTexto("Por gentileza, seu gênero:\n(M): Masculino\t    (F): Feminino\n(T): Transgênero\t(Z): Prefiro não informar.\n");
-        pessoa.genero = scanner.nextLine();
-        pessoa.validarGenero();
+        pessoa.setIdade(scanner.nextByte());
+        imprimirTexto("Seu telefone com DDD de 2 dígitos: ");
+        pessoa.setNumeroTelefone(scanner.next());
+        imprimirTexto("Por gentileza, seu gênero:\n(0): Masculino\t    (1): Feminino\n(2): Transgênero\t(3): Prefiro não informar.\n");
+        Byte generoInformado = scanner.nextByte();
+        pessoa.setGenero(generoInformado);
         imprimirTexto("E para finalizar o cadastro, seu estado civil:\n(1): Solteiro\t(2): Casado\n(3): Divorciado\t(4): Viúvo\n");
-        pessoa.estadoCivil = scanner.nextByte();
+        Byte civilidade = scanner.nextByte();
+        pessoa.setEstadoCivil(civilidade);
         limpaTela();
-        imprimirTexto("Cadastro finalizado!");
+        System.out.println("Cadastro finalizado!\n" + "Nome: " + pessoa.getNome() + " " + pessoa.getSobrenome() + "\nIdade: " + pessoa.getIdade() + " anos\n" + "Contato: " + "DDD (" + pessoa.obterDDD() + ") " + pessoa.telefoneSemDDD() + "\nGênero: " + pessoa.generoToString(String.valueOf(generoInformado)) + "\nEstado civil: " + pessoa.formatarEstadoCivil(String.valueOf(civilidade)));
 
     }
     static void imprimirTexto(String texto) {
@@ -29,7 +28,7 @@ public class ObterInformacoesPessoa {
     }
     static void limpaTela() {
         for(int i = 0; i < 50; i++) {
-            System.out.println("\n");
+            imprimirTexto("\n");
         }
     }
 }
--- RecebendoInformacoes/Pessoa.java
+++ RecebendoInformacoes/Pessoa.java
@@ -4,32 +4,103 @@ public class Pessoa {
     String nome;
     String sobrenome;
     Byte idade;
-    Long numeroTelefone;
-    String genero;
+    String numeroTelefone;
+    Byte genero;
     Byte estadoCivil;
 
     String formarNomeCompleto() {
         return nome + " " + sobrenome;
     }
-
-    Boolean validarIdade() {
-        return idade >= 0 && idade < 120;
-    }
-
     String obterDDD() {
-        String DDD = numeroTelefone.toString(0, 2);
-        return DDD;
+        return numeroTelefone.substring(0, 2);
+    }
+    String telefoneSemDDD() {
+        return numeroTelefone.substring(2, 11);
+    }
+    String generoToString (String generoAPassar) {
+        boolean generoMasculino = genero == 0;
+        boolean generoFeminino = genero == 1;
+        boolean generoTrans = genero == 2;
+        boolean naoInformado = genero == 3;
+        if(generoMasculino) {
+            generoAPassar = "Masculino";
+            System.out.print(generoAPassar);
+        }else if(generoFeminino) {
+             generoAPassar = "Feminino";
+            System.out.println(generoAPassar);
+        }else if(generoTrans) {
+             generoAPassar = "Transgênero";
+            System.out.println(generoAPassar);
+        }
+        else if(naoInformado) {
+             generoAPassar = "Gênero não informado";
+            System.out.println(generoAPassar);
+        }
+        return generoAPassar;
+    }
+    String formatarEstadoCivil (String situacaoAPassar) {
+        boolean solteiro = estadoCivil == 1;
+        boolean casado = estadoCivil == 2;
+        boolean divorciado = estadoCivil == 3;
+        boolean viuvo = estadoCivil == 4;
+        if(solteiro) {
+            situacaoAPassar = "Solteiro";
+            System.out.println(situacaoAPassar);
+        }else if(casado) {
+            situacaoAPassar = "Casado";
+            System.out.println(situacaoAPassar);
+        }else if(divorciado) {
+            situacaoAPassar = "Divorciado";
+            System.out.println(situacaoAPassar);
+        }else if (viuvo) {
+            situacaoAPassar = "Viúvo(a)";
+            System.out.println(situacaoAPassar);
+        }
+        return situacaoAPassar;
     }
 
-    Boolean validarGenero() {
-        boolean generoValido = (genero.equals("F") || genero.equals("M") || genero.equals("T") ||
-                genero.equals("Z") || genero.equals("f") || genero.equals("m") ||
-                genero.equals("t") || genero.equals("z"));
-        if(generoValido) {
-            System.out.println(genero);
-        }else {
-            System.err.println("Gênero inválido! ");
-        }
-        return generoValido;
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public Byte getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Byte idade) {
+        this.idade = idade;
+    }
+
+    public String getNumeroTelefone() {
+        return numeroTelefone;
+    }
+
+    public void setNumeroTelefone(String numeroTelefone) {
+        this.numeroTelefone = numeroTelefone;
+    }
+
+    public Byte getGenero() {
+        return genero;
+    }
+
+    public void setGenero(Byte genero) {
+        this.genero = genero;
+    }
+
+    public Byte getEstadoCivil() {
+        return estadoCivil;
+    }
+
+    public void setEstadoCivil(Byte estadoCivil) {
+        this.estadoCivil = estadoCivil;
     }
 }

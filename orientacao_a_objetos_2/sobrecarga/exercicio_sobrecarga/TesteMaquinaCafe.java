2f752815a0d37133e8bfef47dea0ba9a033f519b
JuanNovais710 <100390136+JuanNovais710@users.noreply.github.com>
2022-05-25 21:45:42 -0300

    Add files via upload

---
+++ orientacao_a_objetos_2/sobrecarga/exercicio_sobrecarga/TesteMaquinaCafe.java
@@ -0,0 +1,16 @@
+package com.company.orientacao_a_objetos_2.sobrecarga.exercicio_sobrecarga;
+
+public class TesteMaquinaCafe {
+    public static void main(String[] args) {
+        MaquinaCafe maquinaCafe = new MaquinaCafe(50);
+        maquinaCafe.fazerCafe();
+        maquinaCafe.fazerCafe();
+        maquinaCafe.fazerCafe();
+        maquinaCafe.fazerCafe();
+        maquinaCafe.fazerCafe();
+        maquinaCafe.fazerCafe();
+        maquinaCafe.fazerCafe();
+
+
+    }
+}

6dcb567f97ef83b3322e9b44337181a8d0e5817d
JuanNovais710 <100390136+JuanNovais710@users.noreply.github.com>
2022-06-01 05:38:26 -0300

    Add files via upload

---
+++ orientacao_a_objetos_2/polimorfismo_e_casting/Conta.java
@@ -0,0 +1,13 @@
+package com.company.polimorfismo_e_casting;
+
+public class Conta {
+    protected double saldo;
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+}
---
+++ orientacao_a_objetos_2/polimorfismo_e_casting/ContaPoupanca.java
@@ -0,0 +1,9 @@
+package com.company.polimorfismo_e_casting;
+
+public class ContaPoupanca extends Conta{
+    private double rendimento = 30;
+
+    public double getRendimento() {
+        return rendimento;
+    }
+}

a9b830cd5563d0d4ea4b3d74b78bb4c521b3fd54
JuanNovais710 <100390136+JuanNovais710@users.noreply.github.com>
2022-06-01 21:38:24 -0300

    Add files via upload

--- orientacao_a_objetos_2/classes_abstratas/exercicio_classe_abstrata/pessoas/contas/ContaAPagar.java
+++ orientacao_a_objetos_2/classes_abstratas/exercicio_classe_abstrata/pessoas/contas/ContaAPagar.java
@@ -11,6 +11,40 @@ Esse método deve exibir todos os detalhes do objeto em um formato legal para ap
 Agora você pode voltar à classe RelatorioContas e invocar o método exibirDetalhes() durante a listagem. Veja que a classe RelatorioContas só deve conhecer o nome do método
 exibirDetalhes(), mais nada!
 Para testar, compile e execute a classe Principal abaixo:
+public static void main(String[] args) {
+		// instanciando fornecedores
+		Fornecedor imobiliaria = new Fornecedor();
+		imobiliaria.setNome("Casa & Cia Negócios Imobiliários");
+		Fornecedor mercado = new Fornecedor();
+		mercado.setNome("Mercado do João");
+
+		// instanciando clientes
+		Cliente atacadista = new Cliente();
+		atacadista.setNome("Triângulo Quadrado Atacadista");
+		Cliente telecom = new Cliente();
+		telecom.setNome("FoneNet Telecomunicações");
+
+		// instanciando contas a pagar
+		ContaPagar contaPagar1 = new ContaPagar();
+		contaPagar1.setDescricao("Aluguel da matriz");
+		contaPagar1.setValor(1230d);
+		contaPagar1.setDataVencimento("10/05/2012");
+		contaPagar1.setFornecedor(imobiliaria);
+		ContaPagar contaPagar2 = new ContaPagar(mercado, "Compras do mês", 390d, "19/05/2012");
+
+		// instanciando contas a receber
+		ContaReceber contaReceber1 = new ContaReceber();
+		contaReceber1.setDescricao("Desenvolvimento de projeto de logística em Java");
+		contaReceber1.setValor(89500d);
+		contaReceber1.setDataVencimento("23/05/2012");
+		contaReceber1.setCliente(atacadista);
+		ContaReceber contaReceber2 = new ContaReceber(telecom, "Manutenção em sistema de conta online",
+			53200d, "13/05/2012");
+		// exibe listagem de todas as contas com detalhamento
+		RelatorioContas relatorio = new RelatorioContas();
+		Conta[] contas = new Conta[]{contaPagar1, contaPagar2, contaReceber1, contaReceber2};
+		relatorio.exibirListagem(contas);
+	}
 */
 
 public class ContaAPagar extends Conta{

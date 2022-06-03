54bc609bb1ef5ed1574bf9825bc83f2a4bde8b24
JuanNovais710 <100390136+JuanNovais710@users.noreply.github.com>
2022-06-02 21:37:00 -0300

    Delete orientacao_a_objetos_2/classes_abstratas/exercicio_classe_abstrata/pessoas directory

--- orientacao_a_objetos_2/classes_abstratas/exercicio_classe_abstrata/pessoas/contas/ContaAReceber.java
+++
@@ -1,4 +0,0 @@
-package com.company.orientacao_a_objetos_2.classes_abstratas.exercicio_classe_abstrata.pessoas.contas;
-
-public class ContaAReceber extends Conta{
-}
--- orientacao_a_objetos_2/classes_abstratas/exercicio_classe_abstrata/pessoas/pessoas/Fornecedor.java
+++
@@ -1,4 +0,0 @@
-package com.company.orientacao_a_objetos_2.classes_abstratas.exercicio_classe_abstrata.pessoas.pessoas;
-
-public class Fornecedor extends Pessoa {
-}

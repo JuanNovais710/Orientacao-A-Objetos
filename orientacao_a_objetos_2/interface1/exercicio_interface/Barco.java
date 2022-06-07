1f609e7048e1864d4e09678d762a135fa5413f4d
JuanNovais710 <100390136+JuanNovais710@users.noreply.github.com>
2022-06-06 21:00:27 -0300

    Add files via upload

---
+++ orientacao_a_objetos_2/interface1/exercicio_interface/Barco.java
@@ -0,0 +1,15 @@
+package com.company.orientacao_a_objetos_2.interface1.exercicio_interface;
+
+public class Barco implements Seguravel{
+    String descricao;
+
+    @Override
+    public void obterDescricao(String descricao) {
+
+    }
+
+    @Override
+    public void calcularValorApolice() {
+
+    }
+}
